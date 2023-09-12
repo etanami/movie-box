3543584a74bf22662a30207c84e0f48c58b6cfc3
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-09-12 20:36:43 +0100

    refactor hero section

--- src/pages/Home/HeroSection.jsx
+++ src/pages/Home/HeroSection.jsx
@@ -1,10 +1,15 @@
+/* eslint-disable react/no-unescaped-entities */
 import Logo from '../../img/logo.png';
 import ham from '../../img/hamburger.png';
+import imdb from '../../img/imdb.png';
+import tomatoes from '../../img/tomatoes.png';
+import play from '../../img/play.png';
+import Background from '../../img/Poster.png'
 
 
 const HeroSection = () => {
   return (
-    <div className='bg-gray-800 w-full py-4 px-24 text-white'>
+    <div className='h-[80vh] w-full bg-cover bg-no-repeat py-4 px-4 sm:px-12 lg:px-24 text-white' style={{backgroundImage: `url(${Background})`}}>
       <nav className='flex justify-between items-center'>
         <div className='flex items-center gap-6 font-bold'>
           <img src={Logo} alt='Home page logo'/>
@@ -18,6 +23,24 @@ const HeroSection = () => {
           </div>
         </div>
       </nav>
+      <div className='flex flex-col gap-4 max-w-sm my-32'>
+        <h1 className='text-5xl font-bold'>John Wick 3 : Parabellum</h1>
+        <div className='flex gap-7'>
+          <div className='text-xs flex gap-2 items-center'>
+            <span><img src={imdb} alt="imdb icon" /></span>
+            <span>86.0/100</span>
+          </div>
+          <div className='text-xs flex gap-2'>
+            <span>97%</span>
+            <span><img src={tomatoes} alt="tomatoes icon" /></span>
+          </div>
+        </div>
+        <p className='text-sm font-medium max-w-xs'>John Wick is on the run after killing a member of the international assassins' guild, and with a $14 million price tag on his head, he is the target of hit men and women everywhere.</p>
+        <div className='bg-rose-700 px-4 py-2 rounded-md flex gap-2 items-center w-48'>
+          <span><img src={play} alt="play icon" /></span>
+          <span className='text-sm font-bold uppercase'>Watch trailer</span>
+        </div>
+      </div>
     </div>
   )
 }
