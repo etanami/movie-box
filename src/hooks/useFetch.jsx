50dfc714facc2555310465a04f5f7b79c6b7881e
Etanami Olatunji <etanamiolatunji@gmail.com>
2023-09-13 16:44:08 +0100

    refactor useFetch hook

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -5,36 +5,33 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  // fetch data from api asychronously
-  const fetchData = async () => {
-    // argument required by the api
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzhkMTM5NDUxNWJlN2E4NTEyNjBjZDA5OGE2MDE3MiIsInN1YiI6IjY0ZmY4YjhkZmZjOWRlMGVlMTc2NmYwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4qTLoFYRKzmXPl6Uirh6qAzzm7NHoPkVtPHhUZ6q_dQ'
-      }
-    };
-    
-    try {
-      const response = await fetch(url, options);
-      // check if requests fails
-      if (!response.ok) {
-        throw new Error("Network response was not OK");
-      }
-      const data = await response.json();
-      console.log(data);
-      setData(data);
-      setIsLoading(false);
-    } catch (error) {
-        setError(error.message);
-        setIsLoading(false);
-    }
-  };
 
   useEffect(() => {
-
-    fetchData();
+    // fetch data from api asychronously
+    async () => {
+      // argument required by the api
+      const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNzhkMTM5NDUxNWJlN2E4NTEyNjBjZDA5OGE2MDE3MiIsInN1YiI6IjY0ZmY4YjhkZmZjOWRlMGVlMTc2NmYwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4qTLoFYRKzmXPl6Uirh6qAzzm7NHoPkVtPHhUZ6q_dQ'
+        }
+      };
+      
+      try {
+        const response = await fetch(url, options);
+        // check if requests fails
+        if (!response.ok) {
+          throw new Error("Network response was not OK");
+        }
+        const data = await response.json();
+        setData(data);
+        setIsLoading(false);
+      } catch (error) {
+          setError(error.message);
+          setIsLoading(false);
+      }
+    };
     
   }, [url]);
 
